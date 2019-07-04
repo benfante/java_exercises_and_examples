560482967ad4ae885e03aaeea140ec6765f3b3e6
Lucio Benfante <lucio@benfante.com>
2019-07-04 15:56:48 +0200

    Added slf4j and logback

---
+++ src/main/java/com/javacourse/examples/LoggingExample.java
@@ -0,0 +1,22 @@
+package com.javacourse.examples;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingExample {
+
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(LoggingExample.class);
+		logger.error("This is an error");
+		logger.warn("This is an warn");
+		logger.info("This is an info");
+		logger.debug("This is an debug");
+		logger.trace("This is an trace");
+		int a = 67;
+		if (logger.isDebugEnabled()) {
+			logger.debug("The value of a is "+a);
+		}
+		logger.debug("The value of a is {}", a);
+	}
+
+}
