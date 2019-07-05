bf5068b8a7dd35582fc57ff01e9d3a264cc105fc
Lucio Benfante <lucio@benfante.com>
2019-07-05 09:46:55 +0200

    Added examples on constructors

---
+++ src/main/java/com/javacourse/examples/DefaultConstructorExample.java
@@ -0,0 +1,34 @@
+package com.javacourse.examples;
+
+public class DefaultConstructorExample {
+	private String anAttribute;
+	private int h;
+	private final int k;
+	
+	{
+		h = 3;
+	}
+	
+	// no more default constructor
+	public DefaultConstructorExample(String anAttribute) {
+		this(anAttribute, 23);
+		this.h = 34;
+	}
+
+	public DefaultConstructorExample(String anAttribute, int k) {
+		this.k = k;
+		this.anAttribute = anAttribute;
+	}
+	
+	public static void main(String[] args) {
+		DefaultConstructorExample instance = new DefaultConstructorExample("pippo");
+		// instance.k = 54; // no, it's final
+		System.out.println(instance);
+	}
+
+	@Override
+	public String toString() {
+		return "DefaultConstructorExample [anAttribute=" + anAttribute + ", h=" + h + "]";
+	}
+
+}
