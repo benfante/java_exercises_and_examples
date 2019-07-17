e2162ef768afcf7b6d3c4e1db9c8a571ff738800
Lucio Benfante <lucio@benfante.com>
2019-07-17 12:44:52 +0200

    Added solution of the JDBC exercise 1 (print rented film of a store in a specified year)

---
+++ src/main/java/com/javacourse/exercises/jdbc/Exercise1.java
@@ -0,0 +1,47 @@
+package com.javacourse.exercises.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Exercise1 {
+	public void printFilms(int storeId, int year) {
+		try (Connection conn = DriverManager
+				.getConnection("jdbc:mysql://localhost/sakila?serverTimezone=UTC&useSSL=false", "root", "root");
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt
+				.executeQuery("SELECT DISTINCT f.film_id, f.title, f.release_year, length FROM film f"
+								+ " JOIN inventory i ON (i.film_id = f.film_id)"
+								+ " JOIN rental r ON (r.inventory_id = i.inventory_id)"
+								+ " WHERE YEAR(r.rental_date) = " + year + " AND i.store_id = " + storeId
+								+ " ORDER BY f.title;");
+				Statement stmt2 = conn.createStatement();) {
+			while (rs.next()) {
+				int id = rs.getInt("film_id");
+				String title = rs.getString("title");
+				int releaseYear = rs.getInt("release_year");
+				int length = rs.getInt("length");
+				try (ResultSet rs2 = stmt2.executeQuery("SELECT a.first_name, a.last_name FROM actor a"
+						+ " JOIN film_actor fa ON (fa.actor_id = a.actor_id)" + " WHERE fa.film_id = " + id + ";")) {
+					System.out.print(title + ", " + releaseYear + ", " + length + " min., (");
+					if (rs2.next()) {
+						System.out.print(rs2.getString(1) + " " + rs2.getString(2));
+						while (rs2.next()) {
+							System.out.print("," + rs2.getString(1) + " " + rs2.getString(2));
+						}
+					}
+					System.out.println(")");
+				}
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Exercise1 app = new Exercise1();
+		app.printFilms(1, 2006);
+	}
+}
