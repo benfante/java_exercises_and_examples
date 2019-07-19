36dd9507592e7699244f69d94ba818a941790f48
Lucio Benfante <lucio@benfante.com>
2019-07-19 08:19:03 +0200

    Added Singleton example

---
+++ src/main/java/com/javacourse/examples/patterns/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package com.javacourse.examples.patterns.singleton;
+
+public class Singleton {
+	private static final Singleton instance;
+
+	static {
+		instance = new Singleton();
+	}
+	
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		return instance;
+	}
+}
+
+class UseSingleton {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+	}
+}
