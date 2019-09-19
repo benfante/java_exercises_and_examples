d3e3b1f791a4ca97034f27ce7358021bb4bb045f
Lucio Benfante <lucio@benfante.com>
2019-09-19 17:15:31 +0200

    Expanded explore string exercise

--- src/main/java/com/javacourse/exercises/base/ExploreString.java
+++ src/main/java/com/javacourse/exercises/base/ExploreString.java
@@ -1,5 +1,7 @@
 package com.javacourse.exercises.base;
 
+import java.util.Arrays;
+
 /**
  * A simple exercise for exploring String methods.
  * 
@@ -16,6 +18,14 @@ public class ExploreString {
 		System.out.println("Primo carattere: "+s.charAt(0));
 		System.out.println("Ultimo carattere: "+s.charAt(s.length()-1));
 		System.out.println("Maiuscolo:"+s.toUpperCase());
+		System.out.println("pluto? " +
+				(s.contains("pluto") ? "YES" : "NO")
+		);
+		System.out.println("pluto? " +
+				(s.indexOf("pluto") > 0 ? "YES" : "NO")
+		);
+		String[] words = s.split(" ");
+		System.out.println(Arrays.toString(words));
 	}
 
 }
