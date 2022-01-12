0be38e99edda555569c528347f50fa820f0f2844
Lucio Benfante <lucio@benfante.com>
2022-01-12 15:31:19 +0100

    Updated to Java 17

--- src/test/java/com/javacourse/exercises/CharTableTest.java
+++ src/test/java/com/javacourse/exercises/CharTableTest.java
@@ -20,7 +20,7 @@ public class CharTableTest {
 		charTable.appendRow(sb, 'a');
 		assertEquals("'a' =  97    'A' =  65", sb.toString());
 	}
-	
+
 	@Test
 	public void testUpperCase() {
 		CharTable charTable = new CharTable();
@@ -30,32 +30,67 @@ public class CharTableTest {
 	@Test
 	public void testFullTable() {
 		CharTable charTable = new CharTable();
-		String expected = "'a' =  97    'A' =  65\n"+
-				"'b' =  98    'B' =  66\n"+
-				"'c' =  99    'C' =  67\n"+
-				"'d' = 100    'D' =  68\n"+
-				"'e' = 101    'E' =  69\n"+
-				"'f' = 102    'F' =  70\n"+
-				"'g' = 103    'G' =  71\n"+
-				"'h' = 104    'H' =  72\n"+
-				"'i' = 105    'I' =  73\n"+
-				"'j' = 106    'J' =  74\n"+
-				"'k' = 107    'K' =  75\n"+
-				"'l' = 108    'L' =  76\n"+
-				"'m' = 109    'M' =  77\n"+
-				"'n' = 110    'N' =  78\n"+
-				"'o' = 111    'O' =  79\n"+
-				"'p' = 112    'P' =  80\n"+
-				"'q' = 113    'Q' =  81\n"+
-				"'r' = 114    'R' =  82\n"+
-				"'s' = 115    'S' =  83\n"+
-				"'t' = 116    'T' =  84\n"+
-				"'u' = 117    'U' =  85\n"+
-				"'v' = 118    'V' =  86\n"+
-				"'w' = 119    'W' =  87\n"+
-				"'x' = 120    'X' =  88\n"+
-				"'y' = 121    'Y' =  89\n"+
+		String expected = "'a' =  97    'A' =  65\n" +
+				"'b' =  98    'B' =  66\n" +
+				"'c' =  99    'C' =  67\n" +
+				"'d' = 100    'D' =  68\n" +
+				"'e' = 101    'E' =  69\n" +
+				"'f' = 102    'F' =  70\n" +
+				"'g' = 103    'G' =  71\n" +
+				"'h' = 104    'H' =  72\n" +
+				"'i' = 105    'I' =  73\n" +
+				"'j' = 106    'J' =  74\n" +
+				"'k' = 107    'K' =  75\n" +
+				"'l' = 108    'L' =  76\n" +
+				"'m' = 109    'M' =  77\n" +
+				"'n' = 110    'N' =  78\n" +
+				"'o' = 111    'O' =  79\n" +
+				"'p' = 112    'P' =  80\n" +
+				"'q' = 113    'Q' =  81\n" +
+				"'r' = 114    'R' =  82\n" +
+				"'s' = 115    'S' =  83\n" +
+				"'t' = 116    'T' =  84\n" +
+				"'u' = 117    'U' =  85\n" +
+				"'v' = 118    'V' =  86\n" +
+				"'w' = 119    'W' =  87\n" +
+				"'x' = 120    'X' =  88\n" +
+				"'y' = 121    'Y' =  89\n" +
 				"'z' = 122    'Z' =  90\n";
 		assertEquals(expected, charTable.table);
 	}
+
+	@Test
+	public void testFullTableJava15() {
+		CharTable charTable = new CharTable();
+		String expected = """
+				'a' =  97    'A' =  65
+				'b' =  98    'B' =  66
+				'c' =  99    'C' =  67
+				'd' = 100    'D' =  68
+				'e' = 101    'E' =  69
+				'f' = 102    'F' =  70
+				'g' = 103    'G' =  71
+				'h' = 104    'H' =  72
+				'i' = 105    'I' =  73
+				'j' = 106    'J' =  74
+				'k' = 107    'K' =  75
+				'l' = 108    'L' =  76
+				'm' = 109    'M' =  77
+				'n' = 110    'N' =  78
+				'o' = 111    'O' =  79
+				'p' = 112    'P' =  80
+				'q' = 113    'Q' =  81
+				'r' = 114    'R' =  82
+				's' = 115    'S' =  83
+				't' = 116    'T' =  84
+				'u' = 117    'U' =  85
+				'v' = 118    'V' =  86
+				'w' = 119    'W' =  87
+				'x' = 120    'X' =  88
+				'y' = 121    'Y' =  89
+				'z' = 122    'Z' =  90
+				""";
+		assertEquals(expected, charTable.table);
+	}
+
 }
