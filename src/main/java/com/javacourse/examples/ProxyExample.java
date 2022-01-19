63850a10e43e767a0930828fe1e435dca7d82096
Lucio Benfante <lucio@benfante.com>
2022-01-19 11:11:55 +0100

    Enhanced invocation handler

--- src/main/java/com/javacourse/examples/ProxyExample.java
+++ src/main/java/com/javacourse/examples/ProxyExample.java
@@ -8,14 +8,23 @@ interface QueryGenerator {
     String findAllPerson();
     String findAllUsers();
     String findAllEmployee();
+    String searchPersonById(long id);
 }
 
 class MyInvocationHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String entity = method.getName().substring(7);
-        return String.format("SELECT * from %s;", entity);
+
+        String name = method.getName();
+        if (name.startsWith("findAll")) {
+            String entity = name.substring(7);
+            return String.format("SELECT * from %s;", entity);
+        } else if (name.startsWith("search")) {
+            int byIndex = name.indexOf("By");
+            return String.format("SELECT * from %s WHERE %s = %s;", name.substring(6, byIndex), name.substring(byIndex + 2), args[0]);
+        }
+        return "Bohhh...";
     }
 
 }
@@ -30,5 +39,6 @@ public class ProxyExample {
         System.out.println(proxy.findAllPerson());
         System.out.println(proxy.findAllUsers());
         System.out.println(proxy.findAllEmployee());
+        System.out.println(proxy.searchPersonById(5));
     }
 }
