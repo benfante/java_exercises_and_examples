15fba164c03f6d00e7436600eb0b15f3a4809982
Lucio Benfante <lucio@benfante.com>
2022-10-11 14:48:19 +0200

    Added simple array example

--- src/main/java/com/javacourse/examples/ArrayExample.java
+++ src/main/java/com/javacourse/examples/ArrayExample.java
@@ -22,6 +22,35 @@ public class ArrayExample {
 		System.out.println(Arrays.toString(a));
 	}
 
+
+
+	public void simpleArray() {
+		int a1 = 12;
+		int a2 = 13;
+		int a3 = 23;
+		int a4 = 54;
+		int a5 = 4;
+
+		int media = (a1 + a2 + a3 + a4 + a5)/5;
+
+		int[] a = new int[] {12, 13, 23, 54, 4};
+
+		int sum = 0;
+		for(int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		int m = sum / a.length;
+	}
+
+
+	public void stringArray() {
+		String[] months = new String[] {
+			"Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio"
+		};
+		months[5] = "Giugno"; // ArrayIndexOutOfBoundsException
+		System.out.println(months[0].toUpperCase());
+	}
+
 	public static void main(String[] args) {
 //		mixValues();
 		enlargeArray();
