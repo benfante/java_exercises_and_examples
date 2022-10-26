d177a4a3a64aee8700dd5ed930c5b173aa9d6960
Lucio Benfante <lucio@benfante.com>
2022-10-26 10:09:53 +0200

    Enhanced with modern Java features

--- src/main/java/com/javacourse/exercises/jdbc/Exercise1.java
+++ src/main/java/com/javacourse/exercises/jdbc/Exercise1.java
@@ -5,7 +5,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Exercise1 {
 	public void printFilms(int storeId, int year) {
@@ -13,27 +16,33 @@ public class Exercise1 {
 				.getConnection("jdbc:mysql://localhost/sakila?serverTimezone=UTC&useSSL=false", "root", "root");
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt
-				.executeQuery("SELECT DISTINCT f.film_id, f.title, f.release_year, length FROM film f"
-								+ " JOIN inventory i ON (i.film_id = f.film_id)"
-								+ " JOIN rental r ON (r.inventory_id = i.inventory_id)"
-								+ " WHERE YEAR(r.rental_date) = " + year + " AND i.store_id = " + storeId
-								+ " ORDER BY f.title;");
+				.executeQuery("""
+					SELECT DISTINCT f.film_id, f.title, f.release_year, length FROM film f
+							JOIN inventory i ON (i.film_id = f.film_id)
+							JOIN rental r ON (r.inventory_id = i.inventory_id)
+						WHERE YEAR(r.rental_date) = %d AND i.store_id = %d
+						ORDER BY f.title;
+					""".formatted(year, storeId));
 				Statement stmt2 = conn.createStatement();) {
 			while (rs.next()) {
 				int id = rs.getInt("film_id");
 				String title = rs.getString("title");
 				int releaseYear = rs.getInt("release_year");
 				int length = rs.getInt("length");
-				try (ResultSet rs2 = stmt2.executeQuery("SELECT a.first_name, a.last_name FROM actor a"
-						+ " JOIN film_actor fa ON (fa.actor_id = a.actor_id)" + " WHERE fa.film_id = " + id + ";")) {
-					System.out.print(title + ", " + releaseYear + ", " + length + " min., (");
-					if (rs2.next()) {
-						System.out.print(rs2.getString(1) + " " + rs2.getString(2));
-						while (rs2.next()) {
-							System.out.print(", " + rs2.getString(1) + " " + rs2.getString(2));
-						}
+				try (ResultSet rs2 = stmt2.executeQuery("""
+					SELECT a.first_name, a.last_name FROM actor a
+							JOIN film_actor fa ON (fa.actor_id = a.actor_id)
+						WHERE fa.film_id = %d;
+					""".formatted(id))) {
+					List<String> actors = new ArrayList<>();
+					while (rs2.next()) {
+						actors.add(String.format("%s %s",
+							rs2.getString(1),
+							rs2.getString(2)));
 					}
-					System.out.println(")");
+					System.out.printf("%s, %d, %d min., (%s)%n",
+						title, releaseYear, length,
+						actors.stream().collect(Collectors.joining(", ")));
 				}
 			}
 		} catch (SQLException ex) {
